47f49d61c47fba1496bb70760557a8a0c9def3bf
aaaelite21 <alexander_ali@my.uri.edu>
2019-06-05 17:27:22 -0400

    added other exchanges

--- AnalyzeFunctions/HitBTCAnalyze.js
+++ AnalyzeFunctions/HitBTCAnalyze.js
@@ -12,35 +12,6 @@ module.exports=function(message){
     if (message.unix) {
         let current = new Date(message.unix);
         let now = new Date(current.getTime() - 1000 * this.delay)
-        if (this.lastMinute !== now.getMinutes()) { // will not change after this
-            this.lastMinute = now.getMinutes();
-            //update time
-            this.currentData.time = toTheMinute(now);
-            this.onMin1(this.currentData.price, this.currentData.time);
-            if (this.lastMinute % 5 === 0) {
-                this.onMin5(this.currentData.price, this.currentData.time)
-            }
-            if (this.lastMinute % 15 === 0) {
-                this.onMin15(this.currentData.price, this.currentData.time)
-            }
-
-            if (this.lastHour !== now.getHours()) {
-                this.lastHour = now.getHours();
-                this.onHour1(this.currentData.price, this.currentData.time);
-                if (this.lastHour % 6 === 0) {
-                    this.onHour6(this.currentData.price, this.currentData.time)
-                }
-
-                if (this.lastDay !== now.getDate()) {
-                    this.lastDay = now.getDate();
-                    this.onDay(this.currentData.price, this.currentData.time)
-                }
-
-                if (this.lasUtcDay !== now.getUTCDate()) {
-                    this.lasUtcDay = now.getUTCDate();
-                    this.onUtcDay(this.currentData.price, this.currentData.time);
-                }
-            }
-        }
+        this.analyzeNewTime(now);
     }
 }
--- gdax-pulse.js
+++ gdax-pulse.js
@@ -62,6 +62,39 @@ class Pulse {
     onDay() {}
     onUtcDay() {}
     onNewPrice() {}
+
+    analyzeNewTime(now){
+        if (this.lastMinute !== now.getMinutes()) { // will not change after this
+            this.lastMinute = now.getMinutes();
+            //update time
+            this.currentData.time = toTheMinute(now);
+            this.onMin1(this.currentData.price, this.currentData.time);
+            if (this.lastMinute % 5 === 0) {
+                this.onMin5(this.currentData.price, this.currentData.time)
+            }
+            if (this.lastMinute % 15 === 0) {
+                this.onMin15(this.currentData.price, this.currentData.time)
+            }
+
+            if (this.lastHour !== now.getHours()) {
+                this.lastHour = now.getHours();
+                this.onHour1(this.currentData.price, this.currentData.time);
+                if (this.lastHour % 6 === 0) {
+                    this.onHour6(this.currentData.price, this.currentData.time)
+                }
+
+                if (this.lastDay !== now.getDate()) {
+                    this.lastDay = now.getDate();
+                    this.onDay(this.currentData.price, this.currentData.time)
+                }
+
+                if (this.lasUtcDay !== now.getUTCDate()) {
+                    this.lasUtcDay = now.getUTCDate();
+                    this.onUtcDay(this.currentData.price, this.currentData.time);
+                }
+            }
+        }
+    }
 }
 
 module.exports = Pulse;
