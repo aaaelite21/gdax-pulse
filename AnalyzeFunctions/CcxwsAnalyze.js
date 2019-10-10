ddc5827849b0d548e8799bc953c9793eed5e7306
alexander ali <alexander_ali@my.uri.edu>
2019-10-09 21:30:03 -0400

    switched ccxws pulse to work off more active ticker

--- AnalyzeFunctions/CcxwsAnalyze.js
+++ AnalyzeFunctions/CcxwsAnalyze.js
@@ -1,15 +1,15 @@
 module.exports = function (message) {
     //update Time
-    if (message.side === 'buy' || message.side === 'sell') { //looking for the most recent price match = when a maker and taker order are paired up
-        let priceOfMatch = parseFloat(message.price);
+    if (message.last) { //looking for the most recent price match = when a maker and taker order are paired up
+        let priceOfMatch = parseFloat(message.last);
         if (priceOfMatch !== this.currentData.price) {
             this.currentData.price = priceOfMatch;
             this.onNewPrice(this.currentData.price, this.currentData.time);
         }
     }
 
-    if (message.unix) {
-        let current = new Date(message.unix);
+    if (message.timestamp) {
+        let current = new Date(message.timestamp);
         let now = new Date(current.getTime() - 1000 * this.delay)
         this.analyzeNewTime(now);
     }
