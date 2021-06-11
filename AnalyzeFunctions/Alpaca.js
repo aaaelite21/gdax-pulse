1093e3ad29e3e077736084bba2333d570cf9547f
aaelite21 <alexander_ali@my.uri.edu>
2021-06-11 15:45:50 -0400

    made changes for v2 of alpaca hq's node js lib

--- AnalyzeFunctions/Alpaca.js
+++ AnalyzeFunctions/Alpaca.js
@@ -1,6 +1,6 @@
 module.exports = function (message) {
   //update Time
-  if (message.ev === "T") {
+  if (message.T === "t") {
     //looking for the most recent price match = when a maker and taker order are paired up
     let priceOfMatch = parseFloat(message.price);
     if (priceOfMatch !== this.currentData.price) {
@@ -11,8 +11,8 @@ module.exports = function (message) {
     this.onMatch(this.currentData.price, this.currentData.time, message);
   }
 
-  if (message.timestamp) {
-    let current = new Date(message.timestamp / 1000000);
+  if (message.Timestamp) {
+    let current = new Date(message.Timestamp);
     let now = new Date(current.getTime() - 1000 * this.delay);
     this.analyzeNewTime(now);
   }
