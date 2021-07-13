ffadb569c8908225126f44500a89d9f29f5a1ead
Alexander <alexander_ali@my.uri.edu>
2021-07-13 18:12:29 -0400

    finally working

--- AnalyzeFunctions/Alpaca.js
+++ AnalyzeFunctions/Alpaca.js
@@ -2,7 +2,7 @@ module.exports = function (message) {
   //update Time
   if (message.T === "t") {
     //looking for the most recent price match = when a maker and taker order are paired up
-    let priceOfMatch = parseFloat(message.price);
+    let priceOfMatch = parseFloat(message.Price);
     if (priceOfMatch !== this.currentData.price) {
       this.currentData.price = priceOfMatch;
       this.onNewPrice(this.currentData.price, this.currentData.time);
