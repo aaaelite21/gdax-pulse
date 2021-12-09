61849e2639cb5d07b6fc3f8603a6d6006fef8daf
Alexander <alexander_ali@my.uri.edu>
2021-12-09 16:40:45 -0500

    added day_of_week to Nyc time conversion

--- Lib/ConvertToExchangeTimes.js
+++ Lib/ConvertToExchangeTimes.js
@@ -6,69 +6,76 @@ const hour_in_ms = 3600000;
 const day_in_ms = 86400000;
 
 /**
- * 
- * @param {Number} day_of_the_first 
+ *
+ * @param {Number} day_of_the_first
  */
- function GetDateOfSecondSunday(day_of_the_first) {
-    if(day_of_the_first === 0) return 8;     //sunday
-    if(day_of_the_first === 1) return 14;    //monday
-    if(day_of_the_first === 2) return 13;    //tuesday
-    if(day_of_the_first === 3) return 12;    //wednesday
-    if(day_of_the_first === 4) return 11;    //thursday
-    if(day_of_the_first === 5) return 10;    //friday
-    if(day_of_the_first === 6) return 9;     //saturday  
+function GetDateOfSecondSunday(day_of_the_first) {
+  if (day_of_the_first === 0) return 8; //sunday
+  if (day_of_the_first === 1) return 14; //monday
+  if (day_of_the_first === 2) return 13; //tuesday
+  if (day_of_the_first === 3) return 12; //wednesday
+  if (day_of_the_first === 4) return 11; //thursday
+  if (day_of_the_first === 5) return 10; //friday
+  if (day_of_the_first === 6) return 9; //saturday
 }
 
 /**
- * 
- * @param {Date} _date 
+ *
+ * @param {Date} _date
  */
-function Nyc(_date){
-    let tz_offset = winter_offset;
+function Nyc(_date) {
+  let tz_offset = winter_offset;
 
-    let now = new Date((typeof _date === "string" ? new Date(_date) : _date));
-    let current_month = now.getUTCMonth();
-    if(current_month > march && current_month <= november){
-        tz_offset = summer_offset;
+  let now = new Date(typeof _date === "string" ? new Date(_date) : _date);
+  let current_month = now.getUTCMonth();
+  if (current_month > march && current_month <= november) {
+    tz_offset = summer_offset;
+  }
+
+  let temp_nyc_time = new Date(now.getTime() - tz_offset * hour_in_ms);
+
+  if (current_month === march || current_month === november) {
+    let day_of_the_month = temp_nyc_time.getUTCDate();
+    let first_of_the_month = new Date(
+      temp_nyc_time.getTime() - (day_of_the_month - 1) * day_in_ms,
+    );
+    let day_of_first = first_of_the_month.getUTCDay();
+    let date_of_second_sunday = GetDateOfSecondSunday(day_of_first);
+
+    if (
+      (current_month === march && date_of_second_sunday < day_of_the_month) ||
+      (current_month === november && date_of_second_sunday > day_of_the_month)
+    ) {
+      tz_offset = summer_offset;
     }
 
-    let temp_nyc_time = new Date(now.getTime() - tz_offset * hour_in_ms);
-
-    if(current_month === march || current_month === november){
-        let day_of_the_month = temp_nyc_time.getUTCDate();
-        let first_of_the_month = new Date(temp_nyc_time.getTime() - (day_of_the_month-1) * day_in_ms);
-        let day_of_first = first_of_the_month.getUTCDay();
-        let date_of_second_sunday = GetDateOfSecondSunday(day_of_first);
-
-        if((current_month === march && date_of_second_sunday < day_of_the_month) || 
-        (current_month === november && date_of_second_sunday > day_of_the_month)){
-            tz_offset = summer_offset;
-        }
-
-        if(current_month === march && date_of_second_sunday === day_of_the_month){
-            let utc_hour = temp_nyc_time.getUTCHours();
-            if(utc_hour >= 2) tz_offset = summer_offset;
-        }
-
-        if(current_month === november && date_of_second_sunday === day_of_the_month){
-            temp_nyc_time = new Date(now.getTime() - summer_offset * hour_in_ms);
-            let utc_hour = temp_nyc_time.getUTCHours();
-            if(utc_hour >= 2) tz_offset = winter_offset;
-        }
+    if (current_month === march && date_of_second_sunday === day_of_the_month) {
+      let utc_hour = temp_nyc_time.getUTCHours();
+      if (utc_hour >= 2) tz_offset = summer_offset;
     }
 
-    let offset_time = new Date(now.getTime() - tz_offset * hour_in_ms);
+    if (
+      current_month === november &&
+      date_of_second_sunday === day_of_the_month
+    ) {
+      temp_nyc_time = new Date(now.getTime() - summer_offset * hour_in_ms);
+      let utc_hour = temp_nyc_time.getUTCHours();
+      if (utc_hour >= 2) tz_offset = winter_offset;
+    }
+  }
 
-    return {
-        year: offset_time.getUTCFullYear(),
-        month: offset_time.getUTCMonth() + 1,
-        day: offset_time.getUTCDate(),
-        hour: offset_time.getUTCHours(),
-        minute: offset_time.getUTCMinutes()
-    };
+  let offset_time = new Date(now.getTime() - tz_offset * hour_in_ms);
+
+  return {
+    year: offset_time.getUTCFullYear(),
+    month: offset_time.getUTCMonth() + 1,
+    day: offset_time.getUTCDate(),
+    hour: offset_time.getUTCHours(),
+    minute: offset_time.getUTCMinutes(),
+    day_of_week: offset_time.getUTCDay(),
+  };
 }
 
-
 module.exports = {
-    Nyc
+  Nyc,
 };
--- test/ExchangeTimeTest.js
+++ test/ExchangeTimeTest.js
@@ -2,105 +2,134 @@ const { Nyc } = require("../index").ConvertToExchangeTimes;
 const assert = require("assert");
 
 describe("Timezone Conversions", () => {
-    describe("Nyc", () => {
-        it("get proper time and hours for new your time", () => {
-            let time = new Date("1/15/2021 14:30 utc");
-            let conversion = Nyc(time);
-            assert.deepStrictEqual(conversion, {
-                day: 15,
-                hour: 9,
-                minute: 30,
-                month: 1,
-                year: 2021
-            });
-        });
-        it("respects 24 hour time", () => {
-            let time = new Date("1/16/2021 02:30 utc");
-            let conversion = Nyc(time);
-            assert.deepStrictEqual(conversion, {
-                day: 15,
-                hour: 21,
-                minute: 30,
-                month: 1,
-                year: 2021
-            });
-        });
-        it("handles date after DST", () => {
-            let time = new Date("4/16/2021 013:30 utc");
-            let conversion = Nyc(time);
-            assert.deepStrictEqual(conversion, {
-                day: 16,
-                hour: 9,
-                minute: 30,
-                month: 4,
-                year: 2021
-            });
-        });
-        it("handles date before DST", () => {
-            let time = new Date("1/16/2021 014:30 utc");
-            let conversion = Nyc(time);
-            assert.deepStrictEqual(conversion, {
-                day: 16,
-                hour: 9,
-                minute: 30,
-                month: 1,
-                year: 2021
-            });
-        });
-        it("handles DST before 2:00 am est on the second sunday of march (UTC-5)",()=>{
-            let time = new Date("3/14/2021 05:00 utc");
-            let conversion = Nyc(time);
-            assert.deepStrictEqual(conversion, {
-                day: 14,
-                hour: 0,
-                minute: 0,
-                month: 3,
-                year: 2021
-            }, "should be UTC -5");
-        });
-        it("handles DST after 2:00 am est on the second sunday of march (UTC-4)",()=>{
-            let time = new Date("3/14/2021 07:30 utc");
-            let conversion = Nyc(time);
-            assert.deepStrictEqual(conversion, {
-                day: 14,
-                hour: 3,
-                minute: 30,
-                month: 3,
-                year: 2021
-            }, "should now be UTC -4");
-        });
-        it("handles DST before 2:00 am est on the second sunday of november (UTC-4)",()=>{
-            let time = new Date("11/8/2020 05:30 utc");
-            let conversion = Nyc(time);
-            assert.deepStrictEqual(conversion, {
-                day: 8,
-                hour: 1,
-                minute: 30,
-                month: 11,
-                year: 2020
-            }, "should be UTC -4 before fall behind");
-        });
-        it("handles DST at 1:00 am est on the second sunday of november roll back (UTC-5)",()=>{
-            let time = new Date("11/8/2020 06:00 utc");
-            let conversion = Nyc(time);
-            assert.deepStrictEqual(conversion, {
-                day: 8,
-                hour: 1,
-                minute: 0,
-                month: 11,
-                year: 2020
-            }, "should now be UTC -5 after fall behind");
-        });
-        it("handles DST after 2:00 am est on the second sunday of november (UTC-5)",()=>{
-            let time = new Date("11/8/2020 07:00 utc");
-            let conversion = Nyc(time);
-            assert.deepStrictEqual(conversion, {
-                day: 8,
-                hour: 2,
-                minute: 0,
-                month: 11,
-                year: 2020
-            }, "should now be UTC -5 after fall behind");
-        });
+  describe("Nyc", () => {
+    it("get proper time and hours for new your time", () => {
+      let time = new Date("1/15/2021 14:30 utc");
+      let conversion = Nyc(time);
+      assert.deepStrictEqual(conversion, {
+        day: 15,
+        hour: 9,
+        minute: 30,
+        month: 1,
+        year: 2021,
+        day_of_week: 5,
+      });
     });
+    it("respects 24 hour time", () => {
+      let time = new Date("1/16/2021 02:30 utc");
+      let conversion = Nyc(time);
+      assert.deepStrictEqual(conversion, {
+        day: 15,
+        hour: 21,
+        minute: 30,
+        month: 1,
+        year: 2021,
+        day_of_week: 5,
+      });
+    });
+    it("handles date after DST", () => {
+      let time = new Date("4/16/2021 013:30 utc");
+      let conversion = Nyc(time);
+      assert.deepStrictEqual(conversion, {
+        day: 16,
+        hour: 9,
+        minute: 30,
+        month: 4,
+        year: 2021,
+        day_of_week: 5,
+      });
+    });
+    it("handles date before DST", () => {
+      let time = new Date("1/16/2021 014:30 utc");
+      let conversion = Nyc(time);
+      assert.deepStrictEqual(conversion, {
+        day: 16,
+        hour: 9,
+        minute: 30,
+        month: 1,
+        year: 2021,
+        day_of_week: 6,
+      });
+    });
+    it("handles DST before 2:00 am est on the second sunday of march (UTC-5)", () => {
+      let time = new Date("3/14/2021 05:00 utc");
+      let conversion = Nyc(time);
+      assert.deepStrictEqual(
+        conversion,
+        {
+          day: 14,
+          hour: 0,
+          minute: 0,
+          month: 3,
+          year: 2021,
+          day_of_week: 0,
+        },
+        "should be UTC -5",
+      );
+    });
+    it("handles DST after 2:00 am est on the second sunday of march (UTC-4)", () => {
+      let time = new Date("3/14/2021 07:30 utc");
+      let conversion = Nyc(time);
+      assert.deepStrictEqual(
+        conversion,
+        {
+          day: 14,
+          hour: 3,
+          minute: 30,
+          month: 3,
+          year: 2021,
+          day_of_week: 0,
+        },
+        "should now be UTC -4",
+      );
+    });
+    it("handles DST before 2:00 am est on the second sunday of november (UTC-4)", () => {
+      let time = new Date("11/8/2020 05:30 utc");
+      let conversion = Nyc(time);
+      assert.deepStrictEqual(
+        conversion,
+        {
+          day: 8,
+          hour: 1,
+          minute: 30,
+          month: 11,
+          year: 2020,
+          day_of_week: 0,
+        },
+        "should be UTC -4 before fall behind",
+      );
+    });
+    it("handles DST at 1:00 am est on the second sunday of november roll back (UTC-5)", () => {
+      let time = new Date("11/8/2020 06:00 utc");
+      let conversion = Nyc(time);
+      assert.deepStrictEqual(
+        conversion,
+        {
+          day: 8,
+          hour: 1,
+          minute: 0,
+          month: 11,
+          year: 2020,
+          day_of_week: 0,
+        },
+        "should now be UTC -5 after fall behind",
+      );
+    });
+    it("handles DST after 2:00 am est on the second sunday of november (UTC-5)", () => {
+      let time = new Date("11/8/2020 07:00 utc");
+      let conversion = Nyc(time);
+      assert.deepStrictEqual(
+        conversion,
+        {
+          day: 8,
+          hour: 2,
+          minute: 0,
+          month: 11,
+          year: 2020,
+          day_of_week: 0,
+        },
+        "should now be UTC -5 after fall behind",
+      );
+    });
+  });
 });
